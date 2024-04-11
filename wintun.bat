6a3c16a1f848ad673df4b1e1ece6985563a88cb6
Rolly Maulana Awangga <awangga@gmail.com>
2024-04-11 07:12:44 +0700

    update

--- wintun.bat
+++ wintun.bat
@@ -21,7 +21,7 @@ echo Optional: Configure bypass list (if needed)
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyOverride /t REG_SZ /d "<local>;*.example.com" /f
 
 echo Proxy settings updated successfully.
-
+ping google.com
 
 echo Starting tap interface with name wintun
 start tun2socks -device wintun -proxy %concatenated%
@@ -32,7 +32,6 @@ set "interface_found=false"
 :check_interface
 echo Use ipconfig to list all network interfaces and detect wintun
 for /f "tokens=1,* delims=:" %%A in ('ipconfig ^| findstr /n "^"') do (
-    echo Check if the line contains "wintun"
     echo %%B | findstr /i "wintun" >nul
     if not errorlevel 1 (
         echo "wintun" interface found.
@@ -40,9 +39,10 @@ for /f "tokens=1,* delims=:" %%A in ('ipconfig ^| findstr /n "^"') do (
     )
 )
 
-echo If "wintun" interface is not found
+echo Check "wintun" interface is not found
 if "%interface_found%"=="false" (
     echo "wintun" interface not found. Check Again......
+	ping google.com
 	goto :check_interface
 ) else (
     echo "wintun" interface found.
@@ -52,19 +52,40 @@ if "%interface_found%"=="false" (
 echo set IP Adress of wintun into 172.16.1.1
 netsh interface ip set address name="wintun" source=static addr=172.16.1.1 mask=255.255.255.0 gateway=none
 
+
+set "ip_found=false"
 :check_wintunip
 echo Use ipconfig to get IP address of "wintun" interface
 for /f "tokens=2 delims=:" %%A in ('ipconfig ^| findstr /C:"wintun" /C:"172.16.1.1"') do (
-	echo Winwun IP set to :
+	echo Wintun IP set to :
     echo %%A
-)else{
+	set "ip_found=true"
+) 
+
+echo Check "wintun" IP is not set
+if "%ip_found%"=="false" (
+    echo IP Wintun not set up. Check Again......
+	ping google.com
 	goto :check_wintunip
-}
+) else (
+    echo "wintun" IP has set successfully.
+)
 
 echo add default gateway via wintun
+ping google.com
 route add 0.0.0.0 mask 0.0.0.0 172.16.1.1 metric 5
+ping google.com
 
-echo Please enter your name:
-set /p username=
-echo Hello, %username%! Welcome to the script.
-
+echo 
+echo ================================================================
+echo UNTUK MENUTUP KONEKSI INTERNET:
+echo CLOSE DAHULU CMD tun2socks.exe KEMUDIAN
+echo TEKAN SEMBARANG UNTUK DISKONEK DARI KONEKSI INTERNET
+echo ================================================================
+echo
+pause
+ping google.com
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyEnable /t REG_DWORD /d 0 /f
+ping google.com
+netsh interface ip delete destinationcache
+pause
